42fb398949ee5716ac332f7e1a0d73698d403eee
Kerry Su <me@sshockwave.net>
2023-01-26 13:04:20 +0800

    Use cdn from bytedance

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -29,5 +29,6 @@ document$.subscribe(() => {
 // Load cpp_runner
 
 document$.subscribe(() => {
+  ace.config.set('basePath', 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/ace/1.4.14/');
   document.querySelectorAll('.run code').forEach(createRunner);
 });
