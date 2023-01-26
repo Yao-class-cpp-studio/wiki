585d513e08d52ba72396afe41eff2fe20f94c510
Kerry Su <me@sshockwave.net>
2023-01-26 13:09:23 +0800

    Refactor use katex instead of mathjax

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,24 +1,16 @@
-// Load Mathjax
-
-window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
-
+// Load Katex
 document$.subscribe(() => { 
-  MathJax.typesetPromise()
+  document.querySelectorAll('.arithmatex').forEach((el) => {
+    const tex = el.textContent || el.innerText;
+    if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+      katex.render(tex.slice(2, -2), el, { 'displayMode': false });
+    } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+      katex.render(tex.slice(2, -2), el, { 'displayMode': true });
+    }
+  });
 })
 
 // Load Asciinema
-
 document$.subscribe(() => {
   document.querySelectorAll('div[data-asciinema]').forEach((el) => {
     console.log(el);
@@ -27,7 +19,6 @@ document$.subscribe(() => {
 });
 
 // Load cpp_runner
-
 document$.subscribe(() => {
   ace.config.set('basePath', 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/ace/1.4.14/');
   document.querySelectorAll('.run code').forEach(createRunner);
